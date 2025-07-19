40822b0a04c48b54249133c3242ddccdb426a3ce
chan-max <jackieontheway666@gmail.com>
2025-07-19 09:18:06 +0800

    dev

--- test-deploy-flow.sh
+++ test-deploy-flow.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+###
+ # @Author: chan-max jackieontheway666@gmail.com
+ # @Date: 2025-07-19 09:14:56
+ # @LastEditors: chan-max jackieontheway666@gmail.com
+ # @LastEditTime: 2025-07-19 09:16:36
+ # @FilePath: /design-server/Users/jackie/yishe-electron/test-deploy-flow.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
 
 # 测试部署流程脚本
 # 用于验证更新脚本和部署配置是否正确
